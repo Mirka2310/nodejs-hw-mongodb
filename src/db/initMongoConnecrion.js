1d74f97a09675f1ff58cda4e7e70c75e1c3bb5a9
Myroslava <gordinskamiroslava@gmail.com>
2024-06-25 14:36:07 +0200

    add

--- src/db/initMongoConnecrion.js
+++ src/db/initMongoConnecrion.js
@@ -7,20 +7,14 @@ export const initMongoConnection = async () => {
   const url = env('MONGODB_URL');
   const db = env('MONGODB_DB');
 
-  if (!user || !pwd || !url || !db) {
-    throw new Error('Missing necessary environment variables for MongoDB connection');
-  }
-
-  const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=MyCluster`;
-
   try {
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=MyCluster`,
+    );
+
     console.log('Mongo connection successfully established!');
   } catch (error) {
-    console.error('Error while setting up mongo connection', error);
+    console.log('Error while setting up mongo connection', error);
     throw error;
   }
 };
