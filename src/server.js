12270fecdda4c8d2b6cf6efbc001ec15cc779347
Myroslava <gordinskamiroslava@gmail.com>
2024-07-09 10:54:46 +0200

    add

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactsById } from './services/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandlers.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -12,13 +11,14 @@ import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-const PORT = Number(env('PORT', '3007'));
+const PORT = Number(env('PORT', '3002'));
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
@@ -27,19 +27,19 @@ export const setupServer = () => {
       },
     }),
   );
+
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello World!',
     });
   });
+
   app.use(router);
-  app.use(contactsRouter);
   app.use('/contacts', contactsRouter);
 
+  app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  app.use(('*', notFoundHandler));
-  app.use(cookieParser());
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
