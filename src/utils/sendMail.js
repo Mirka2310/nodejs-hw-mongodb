bd105b4d2ce41190c01f943a274c859a3035fada
Myroslava <gordinskamiroslava@gmail.com>
2024-07-26 15:42:15 +0200

    +++

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,35 +1,17 @@
 import nodemailer from 'nodemailer';
+
 import { SMTP } from '../constants/index.js';
 import { env } from './env.js';
 
-// транспортувальника Nodemailer
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
-  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
 });
 
-// Функція для надсилання електронних листів з логуванням
 export const sendEmail = async (options) => {
-  try {
-    // Логування параметрів запиту
-    console.log('Sending email with options:', options);
-
-    // Надсилання електронного листа
-    const info = await transporter.sendMail(options);
-
-    // Логування результату надсилання
-    console.log('Email sent:', info);
-
-    return info;
-  } catch (error) {
-    // Логування помилки
-    console.error('Error sending email:', error);
-
-    throw new Error('Failed to send email');
-  }
+  return await transporter.sendMail(options);
 };
